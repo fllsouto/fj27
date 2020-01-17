563baca35cadcc7a06416d583dc454b2482ae635
Fellipe Souto <fllsouto@gmail.com>
2020-01-17 16:04:20 -0300

    Errata Resposta Exe 4.8.12

--- forum/src/main/java/br/com/alura/forum/controller/TopicController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -5,18 +5,17 @@ import br.com.alura.forum.model.Category;
 import br.com.alura.forum.model.Course;
 import br.com.alura.forum.model.User;
 import br.com.alura.forum.model.topic.domain.Topic;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicController {
 
-    @ResponseBody
-    @RequestMapping("/api/topics")
+    @GetMapping(value = "/api/topics", produces=MediaType.APPLICATION_JSON_VALUE)
     public List<TopicBriefOutputDto> listTopics() {
         Category subcategory = new Category("Java", new Category("Programação"));
         Course course = new Course("Java e JSF", subcategory);
