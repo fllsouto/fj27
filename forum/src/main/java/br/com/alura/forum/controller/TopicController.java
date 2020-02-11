6226fd1982ace962ee0843625b843b4562d8fef3
Fellipe Souto <fllsouto@gmail.com>
2020-02-11 16:31:16 -0300

    Resposta Exe 5.5

--- forum/src/main/java/br/com/alura/forum/controller/TopicController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -5,6 +5,8 @@ import br.com.alura.forum.model.Category;
 import br.com.alura.forum.model.Course;
 import br.com.alura.forum.model.User;
 import br.com.alura.forum.model.topic.domain.Topic;
+import br.com.alura.forum.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +17,13 @@ import java.util.List;
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicRepository topicRepository;
+
     @GetMapping(value = "/api/topics", produces=MediaType.APPLICATION_JSON_VALUE)
     public List<TopicBriefOutputDto> listTopics() {
-        Category subcategory = new Category("Java", new Category("Programação"));
-        Course course = new Course("Java e JSF", subcategory);
-        Topic topic = new Topic("Problemas com JSF",
-                "Erro ao fazer conversão da data",
-                new User("Fulano", "fulano@gmail.com", "123456"), course);
 
-        List<Topic> topics = Arrays.asList(topic, topic, topic);
+        List<Topic> topics = topicRepository.findAll();
         return TopicBriefOutputDto.listFromTopics(topics);
     }
 }
